2c0e6e9cd89c1c89df3c7a9295daedf1811f0c0b
aljomar83 <alberto.marcano@hotmail.com>
2023-11-27 18:48:36 -0300

    arreglos primera parte

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,7 +13,7 @@ const Header=()=>{
         <Container as='header' h={{base:'10vh',md:'14vh'}} maxW="100vw" display="flex" flexDirection="row" justifyContent={'flex-start'}
             bg={'rgb(56, 77, 170)'} alignItems={'center'} >
                 <Container display="flex"  flexDirection="row" alignItems={'center'} justifyContent={'space-around'} gap={{base:'2',sm:'4',md:'4',lg:'6'}} >
-                    <Container fontSize={{base:'20',sm:'30',md:'36',lg:'46'}} color={'whitesmoke'} maxW={{base:'30',md:'50'}}><motion.div whileHover={{ scale: 1.2 , color:'rgb(249, 231, 159)' }}><FontAwesomeIcon icon={faIdCard} flip  /></motion.div></Container>
+                    <Container fontSize={{base:'20',sm:'30',md:'36',lg:'46'}} color={'whitesmoke'} maxW={{base:'30',md:'50'}}><motion.div whileHover={{ scale: 1.2 , color:'rgb(249, 231, 159)' }}></motion.div></Container>
                     <Container><Heading as='h1' maxW={{base:'120',sm:'160',md:'200',lg:'260'}} fontFamily={'Raleway'} fontWeight={'800'} fontSize={{base:'16',sm:'26',md:'36',lg:'46'}} textColor={'white'}>
                         <Link  _hover={{ textDecoration:'none'}} maxW={{base:'120',sm:'160',md:'200',lg:'260'}}> Formulario </Link></Heading></Container>
                 </Container>
